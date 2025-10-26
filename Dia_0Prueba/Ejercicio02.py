a9f69fc46ed9b31b8ccb088b49e6b31127fade2c
luisluiswii-Dev <luissanchezgonzale95@gmail.com>
2025-10-26 21:22:54 +0100

    pruebas de clase

---
+++ Dia_0Prueba/Ejercicio02.py
@@ -0,0 +1,15 @@
+'''
+Hola este es un comentario multi linea
+A
+A
+A
+'''
+# * este es un comentario unilinea
+
+pepe = "cocinero"
+herramientas_de_cocina = 9
+_013 = "dato con numero al principio gracias a _    "
+if False:
+    print("True")
+else:
+    print("False")
